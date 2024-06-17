aade8be9525ddee31ad546f4d93fd7991e62cb7f
Julien Sorel <uria.webdev@gmail.com>
2024-06-17 17:30:30 +0200

    Header + Footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ContactComponent } from './contact/contact.component';
+import { AproposComponent } from './apropos/apropos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AccueilComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'a-propos', component: AproposComponent }
+];
